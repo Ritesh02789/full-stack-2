1601acaaac75d529d55f14ba3b19ebb35928c1fe
Ritesh02789 <your-email@example.com>
2026-09-01 10:23:23 +0530

    Remove EXP 4 Interactive Calendar

--- EXP 4 Interactive Calander/src/mocks/browser.js
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { handlers } from './handlers.js';
-
-// This configures a Service Worker that intercepts requests made from the
-// browser (used only in the app itself, not in tests).
-export const worker = setupWorker(...handlers);
